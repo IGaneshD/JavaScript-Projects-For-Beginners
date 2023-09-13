2013ba2f4a90a251016d566da304bdaf66631665
Ganesh Deshmukh <deshmukh.ganesh.f@gmail.com>
2023-09-13 15:56:49 +0530

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,29 @@
+let inputBox = document.getElementById('inputBox')
+let buttons = document.querySelectorAll('button')
+
+let string = ''
+
+buttons.forEach(element =>{
+    element.addEventListener('click', (b)=>{
+        if(b.target.innerText == '='){
+            string = String(eval(string))
+            inputBox.value = string;
+        }
+        else if (b.target.innerText == 'AC'){
+            string = ''
+            inputBox.value = string;
+        }
+        else if(b.target.innerText == 'DEL'){
+            string = string.substring(0,string.length-1)
+            inputBox.value = string;
+        }
+        else if(b.target.id == 'plusMinus'){
+            string = String(-eval(string))
+            inputBox.value = string;
+        }
+        else{
+            string += b.target.innerText
+            inputBox.value = string
+        }
+    })
+})
